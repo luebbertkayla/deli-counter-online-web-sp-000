b9820ecbd7fbebf3ea6cef7c5f59702d916d69b6
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-01 02:21:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,9 @@ katz_deli = []
 def line(array)
   number = 1
   if array.length > 0 
+    new_array = []
     array.each do |name|
+      new_array << ("#{number}. #{name}")
     puts "The line is currently: #{number}. #{name}"
   number += 1
   else
