40e275373faf06f963bb3226016b5542908522db
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-01 02:10:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,9 @@
 katz_deli = []
 
 def line(array)
-  puts "The line is currently empty."
+  if array.length > 0 
+    puts "The line is currently: #{number}. #{name}"
+  else
+    puts "The line is currently empty."
+  end
 end
